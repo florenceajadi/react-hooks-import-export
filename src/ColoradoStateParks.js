8c39f5af01c4650a37d81731d1a92152ee8e94c0
florence ajadi <florence.ajadi@yahoo.com>
2022-06-07 13:16:47 -0500

    commit tree 4d87df0296b9f6020cde250f289a8ac167387783

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,24 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
+  <div>
+    <MesaVerde />
+  </div> 
 }
+
+
+console.log(RMFunctions.trees)
+//Aspen and Pine
+RMFunctions.wildlife();
+//Elk, Bighorn Sheep, Moose
+// RMFunctions.elevation();
+// Attempted import error
+
+export default ColoradoStateParks;
